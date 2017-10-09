e490e2f0d3f4793dae29a988b975e05af7551c1d
vuthihuong <vuthihuong161996@gmail.com>
2017-10-09 11:34:18 -0700

    database

--- protected/data/Dump20171009.sql
+++ protected/data/Dump20171009.sql
@@ -18,7 +18,6 @@
 --
 -- Table structure for table `user`
 --
-
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
